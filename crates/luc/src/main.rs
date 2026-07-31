c57e71c93af411feeecf0a7af9f2378d088fe0e4
Luciano Correia <luciano.correia@gmail.com>
2026-07-31 14:20:06 +0400

    Implement main function to handle command-line arguments and add run function to read source files; add example script for Luc language.

--- crates/luc/src/main.rs
+++ crates/luc/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Luc");
+use std::{env, fs, io, process::ExitCode};
+
+fn main() -> ExitCode {
+    let mut arguments = env::args();
+    let _program_name = arguments.next();
+
+    let source_path = match arguments.next() {
+        Some(source_path) => source_path,
+        None => {
+            eprintln!("Usage: luc <source_path>");
+            return ExitCode::FAILURE;
+        }
+    };
+
+    match run(source_path.as_str()) {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(error) => {
+            eprintln!("Error: {error}");
+            ExitCode::FAILURE
+        }
+    }
+}
+
+fn run(source_path: &str) -> Result<(), io::Error> {
+    let source = fs::read_to_string(source_path)?;
+    println!("{source}");
+    Ok(())
 }
